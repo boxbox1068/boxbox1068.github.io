c710a3bb16bac82ef2817dd38fa6ef11bf18ac0f
shikaku1068 <shikaku1068@gmail.com>
2020-10-17 01:13:05 +0900

    Update service-worker.js

--- office-in-mirror/service-worker.js
+++ office-in-mirror/service-worker.js
@@ -8,6 +8,7 @@
   self.addEventListener('install', e => {
     e.waitUntil(
       caches.open(cacheName).then(cache => {
+console.log('install');
         return cache.addAll(urlsToCache);
       });
     );
@@ -15,6 +16,7 @@
   self.addEventListener('fetch', e => {
     e.respondWith(
       caches.match(e.request).then(response => {
+console.log('fetch');
         return response || fetch(e.request);
       })
     );
