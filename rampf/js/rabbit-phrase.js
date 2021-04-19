e8ae955c5fa7dbbadd5fcc87d21069b14120cb5f
shikaku1068 <shikaku1068@gmail.com>
2021-04-20 00:16:20 +0900

    Update rabbit-phrase.js

--- rampf/js/rabbit-phrase.js
+++ rampf/js/rabbit-phrase.js
@@ -2,12 +2,16 @@ class RabbitPhrase {
   constructor({src, variables, seed} = {}) {
     src = typeof src == 'string' ? src : '';
     variables = typeof variables == 'object' ? variables : {};
-    seed = Number.isInteger(seed) && seed != 0 ? seed : Math.floor(Math.MAX_SAFE_INTEGER * Math.random()) + 1;
+    seed = Number.isInteger(seed) && seed != 0 ? seed : Math.floor(88675123 * Math.random()) + 1;
     for (const name in variables) {
       src = src.replace(new RegExp(`%${name}%`, 'ig'), variables[name]);
     }
     this._src = src;
     this._seed = seed;
+    this._parsePhrase();
+  }
+  alternate() {
+    this._seed = this._xorshift(this._seed, 1);
     this._parsePhrase();
   }
   _xorshift(seed, count) {
@@ -28,15 +32,17 @@ class RabbitPhrase {
     this._branchItems = [];
     let text = '';
     let html = '';
+    let totalPatterns = 1;
     this._src.replace(/(.*?)(\^+)\{((\\}|.)*?)\}|(.*)/g, (match, p1, p2, p3, p4, p5, offset, string) => {
       if (p5 === undefined) {
         const branchNumber = p2.length - 1;
-        const items = p3.trim().split('|');
+        const items = p3.split('|');
         const index = this._xorshift(this._seed, branchNumber) % items.length;
         const branchItem = items[index];
         this._branchItems[branchNumber] = branchItem;
         text += `${p1}${branchItem}`;
         html += `${p1}<span class="branch" id="branch_${branchNumber}">${branchItem}</span>`;
+        totalPatterns *= items.length;
       } else {
         text += p5;
         html += p5;
@@ -44,6 +50,10 @@ class RabbitPhrase {
     });
     this._text = text;
     this._html = html;
+    this._totalPatterns = totalPatterns;
+  }
+  get src() {
+    return this._src;
   }
   get seed() {
     return this._seed;
@@ -51,13 +61,13 @@ class RabbitPhrase {
   get branchItems() {
     return this._branchItems;
   }
-  get src() {
-    return this._src;
-  }
   get text() {
     return this._text;
   }
   get html() {
     return this._html;
+  }
+  get totalPatterns() {
+    return this._totalPatterns;
   }
 }
