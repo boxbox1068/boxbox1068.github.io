6018859fbb51c251180f9518888f53bcb67ced0e
shikaku1068 <shikaku1068@gmail.com>
2021-04-19 23:37:14 +0900

    Update rabbit-phrase.js

--- ramph/js/rabbit-phrase.js
+++ ramph/js/rabbit-phrase.js
@@ -1,13 +1,13 @@
 class RabbitPhrase {
-  constructor({src, variables, randomSeed} = {}) {
+  constructor({src, variables, seed} = {}) {
     src = typeof src == 'string' ? src : '';
     variables = typeof variables == 'object' ? variables : {};
-    randomSeed = Number.isInteger(randomSeed) && randomSeed != 0 ? randomSeed : 886751;
+    seed = Number.isInteger(seed) && seed != 0 ? seed : Math.floor(Math.MAX_SAFE_INTEGER * Math.random()) + 1;
     for (const name in variables) {
       src = src.replace(new RegExp(`%${name}%`, 'ig'), variables[name]);
     }
     this._src = src;
-    this._randomSeed = randomSeed;
+    this._seed = seed;
     this._parsePhrase();
   }
   _xorshift(seed, count) {
@@ -32,7 +32,7 @@ class RabbitPhrase {
       if (p5 === undefined) {
         const branchNumber = p2.length - 1;
         const items = p3.trim().split('|');
-        const index = this._xorshift(this._randomSeed, branchNumber) % items.length;
+        const index = this._xorshift(this._seed, branchNumber) % items.length;
         const branchItem = items[index];
         this._branchItems[branchNumber] = branchItem;
         text += `${p1}${branchItem}`;
@@ -45,8 +45,8 @@ class RabbitPhrase {
     this._text = text;
     this._html = html;
   }
-  get randomSeed() {
-    return this._randomSeed;
+  get seed() {
+    return this._seed;
   }
   get branchItems() {
     return this._branchItems;
