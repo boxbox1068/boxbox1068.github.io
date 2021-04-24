08f85664395ea2778f8f2342976ab56b1b31b254
shikaku1068 <shikaku1068@gmail.com>
2021-04-24 18:56:20 +0900

    Minor update

--- assets/js/add-copy-button.js
+++ assets/js/add-copy-button.js
@@ -2,7 +2,7 @@ window.addEventListener('load', e => {
   for (const element of document.querySelectorAll('.s1068_add_copy_button')) {
 alert(1);
     const button = document.createElement('button');
-    button.innerText('Copy text to clipboard');
+    button.innerHTML = 'Copy text to clipboard';
     button.addEventListener('click', () => {
       const text = element.innerText;
       navigator.clipboard.writeText(text).then(() => {
