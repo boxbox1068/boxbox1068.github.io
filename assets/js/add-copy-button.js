42fff1fb8072b13c332a5d45002765b3b6504a49
shikaku1068 <shikaku1068@gmail.com>
2021-04-24 18:28:34 +0900

    update

---
+++ assets/js/add-copy-button.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', () => {
+  for (element of document.querySelector('.1068_add_copy_button')) {
+    const button = document.createElement('button');
+    button.innerText('Copy text to clipboard');
+    button.addEventListener('click', () => {
+      const text = element.innerText;
+      navigator.clipboard.writeText(text).then(() => {
+        window.alert('Copy succeeded.');
+      }).catch(e => {
+        window.prompt('Copy failed. Please copy the text below alternatively.', text);
+      });
+    });
+    element.after(button);
+  }
+});
