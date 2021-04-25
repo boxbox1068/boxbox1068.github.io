486ea831c82f7b73a983a266a502f518b0827503
shikaku1068 <shikaku1068@gmail.com>
2021-04-25 09:12:54 +0900

    Minor update

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,21 @@
-window.addEventListener('load', e => {
+'use strict';
+const SITE_MAP = {
+  '': 'ホーム',
+  '/anytime-mirror': 'いつでもミラー',
+  '/bookmarklet': 'ブックマークレット'
+};
+window.addEventListener('DOMContentLoaded', e => {
+  document.head.appendChild((() => {
+    const e = document.createElement('meta');
+    e.setAttribute('name', 'format-detection');
+    e.setAttribute('content', 'telephone=no,address=no,email=no');
+    return e;
+  })());
+  document.body.querySelector('*').before((() => {
+    const e = document.createElement('nav');
+    const path = [];
+    let url = location.href.replace(/^https:\/\/.+?\/|\/.*?$/ig, '');
+alert(url);
+  })());
   document.title += ' - しかくのサイト';
 });
